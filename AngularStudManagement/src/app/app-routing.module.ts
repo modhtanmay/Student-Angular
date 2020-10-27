9e15477fc8f22fc1abe7144f221f878a395dcf73
modhtanmay <modhtanmay@gmail.com>
2020-10-27 17:57:13 +0530

    Routing added

--- AngularStudManagement/src/app/app-routing.module.ts
+++ AngularStudManagement/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentCreateComponent } from './student/student-create/student-create.component';
+import { StudentListComponent } from './student/student-list/student-list.component';
+import { StudentComponent } from './student/student.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: 'students', pathMatch: 'full' },
+    { path: 'students', component: StudentListComponent },
+    { path: 'addStudent', component: StudentCreateComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
