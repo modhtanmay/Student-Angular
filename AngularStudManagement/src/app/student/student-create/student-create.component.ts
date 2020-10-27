fa67de6fb74f485540e1ec0f54a713f2d7f4c617
modhtanmay <modhtanmay@gmail.com>
2020-10-27 06:38:21 +0530

    student-create.component.ts and student-create.component.html updated

--- AngularStudManagement/src/app/student/student-create/student-create.component.ts
+++ AngularStudManagement/src/app/student/student-create/student-create.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Student } from '../student.model';
+import { StudentService } from '../student.service';
 
 @Component({
-  selector: 'app-student-create',
-  templateUrl: './student-create.component.html',
-  styleUrls: ['./student-create.component.css']
+    selector: 'app-student-create',
+    templateUrl: './student-create.component.html',
+    styleUrls: ['./student-create.component.css']
 })
 export class StudentCreateComponent implements OnInit {
 
-  constructor() { }
+    student: Student = new Student();
+    submitted = false;
 
-  ngOnInit(): void {
-  }
+    constructor(private studentService: StudentService) { }
+
+    ngOnInit(): void {
+    }
+
+    newStudent(): void {
+        this.submitted = false;
+        this.student = new Student();
+    }
+    save() {
+        this.studentService.createStudent(this.student).subscribe(data => console.log(data), error => console.log(error));
+        this.student = new Student();
+    }
+
+    onSubmit() {
+        this.submitted = true;
+        this.save();
+    }
+
+
 
 }
